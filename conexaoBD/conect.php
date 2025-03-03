9aaa040f019ee4499a0d2e1492245c404c9beb02
Gabriel de souza barros <gsb4665004@gmail.com>
2025-03-03 19:33:46 -0300

    conectbd create

---
+++ conexaoBD/conect.php
@@ -0,0 +1,13 @@
+<?php
+    $dns = "pgsql:dbname=postgres;host=192.168.1.32";
+    $password = "gabriel";
+    $port = "5432";
+    $user = "postgres";
+    try {
+        
+        $conect = new PDO($dns, $user, $password);
+        echo "User concted in sucess!";
+    }catch(PDOException $error){
+        die($error->getMessage());
+    }
+?>
