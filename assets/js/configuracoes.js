94cfabee1e81c805c93de88f4c08586888a41802
Gabriel <gsb4665004@gmail.com>
2026-02-10 12:00:50 -0300

    Adicionado nova verificação de finalizar o pedido e baixar no estoque e tambem verificar se a quantidade no estoque, vai bater com a quantidade vendida, se caso a quantidade vendida for maior que a quantidade em estoque o pedido não é fechado e ele manda um alerta falando que a quantidade está incorreta e pedi que você confira e troca de senhas

--- assets/js/configuracoes.js
+++ assets/js/configuracoes.js
@@ -36,10 +36,9 @@ document.addEventListener("DOMContentLoaded", async function () {
       document.getElementById("nome").value = userData.ia_nomeusuario || "";
       document.getElementById("sobrenome").value = userData.ia_sobrenome || "";
       document.getElementById("email").value = userData.ia_email || "";
-      document.getElementById("telefone").value = userData.ia_telefone || "";
-      document.getElementById("endereco").value = userData.ia_endereco || "";
-      document.getElementById("dataNasc").value =
-        userData.ia_datanascimento || "";
+      document.getElementById("telefone").value = userData?.ia_numero || "";
+      document.getElementById("endereco").value = userData?.ia_endereco || "";
+      document.getElementById("dataNasc").value = userData?.ia_datanascimento || "";
     }
   } catch (error) {
     console.error("Erro ao carregar dados do usuário:", error);
@@ -88,3 +87,74 @@ document
       alert("Erro ao tentar salvar as alterações.");
     }
   });
+
+document.getElementById('btnCollapse').addEventListener('click', function () {
+  let icon = document.getElementById('arow');
+  icon.innerHTML = icon.innerHTML === '<i class="bi bi-chevron-down"></i>' ? '<i class="bi bi-chevron-up"></i>' : '<i class="bi bi-chevron-down"></i>';
+})
+
+document.getElementById('mostrarSenha').addEventListener('click', function () {
+  let senha = document.getElementById('senha');
+  let senha2 = document.getElementById('passNow');
+  let senha3 = document.getElementById('passConfirm');
+  if (senha.type === 'password') {
+    senha.type = 'text';
+    senha2.type = 'text';
+    senha3.type = 'text';
+  } else {
+    senha.type = 'password';
+    senha2.type = 'password';
+    senha3.type = 'password';
+  }
+})
+
+document.getElementById('alterarPass').addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  let form = new FormData(event.target);
+
+  let senha = form.get('senha');
+  let passNow = form.get('passNow');
+  let passConfirm = form.get('passConfirm');
+  let userId = localStorage.getItem('id');
+
+  console.log(senha, passNow, passConfirm);
+
+  if (passNow !== passConfirm) {
+    let r =document.getElementById('res');
+    r.innerHTML = 'A nova senha não é igual a senha da confirmação!';
+    r.classList.add('alert', 'alert-danger');
+    setTimeout(() => {
+      r.innerHTML = '';
+      r.classList.remove('alert', 'alert-danger');
+    }, 3000);
+    return;
+  }
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/updatepassword/${userId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("acessToken")}`,
+      },
+      body: JSON.stringify({
+        senha,
+        passNow,
+        passConfirm
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      alert(result.output);
+      document.getElementById('alterarPass').reset();
+    } else {
+      alert("Erro ao atualizar: " + (result.status || "Erro desconhecido"));
+    }
+  } catch (error) {
+    console.error("Erro na atualização:", error);
+    alert("Erro ao tentar salvar as alterações.");
+  }
+})
